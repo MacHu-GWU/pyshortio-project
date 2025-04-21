234e02776b95f76771a0381017ebaa632e1bd2b4
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-04-20 23:32:38 -0400

    fix ci

--- pyshortio/tests/client.py
+++ pyshortio/tests/client.py
@@ -8,8 +8,8 @@ from ..client import Client
 IS_CI = "CI" in os.environ
 
 if IS_CI:
+    client = None
+else:
     path_token = Path.home() / ".short.io" / "sanhehu_esc" / "sanhe-dev.txt"
     token = path_token.read_text().strip()
     client = Client(token=token)
-else:
-    client = None
